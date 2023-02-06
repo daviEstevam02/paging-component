79312e97309d1bb6799839a6c969f24a94be35db
EnzoFNGodoy <enzofngodoy@hotmail.com>
2023-02-06 00:12:27 -0300

    Adding the IBaseRepository interface

---
+++ src/EasyDocs.Domain.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,9 @@
+﻿using EasyDocs.Domain.Core.Entities;
+
+namespace EasyDocs.Domain.Core.Interfaces;
+
+public interface IBaseRepository<T> where T : Entity
+{
+    Task Add(T entity);
+    Task Update(T entity);
+}
