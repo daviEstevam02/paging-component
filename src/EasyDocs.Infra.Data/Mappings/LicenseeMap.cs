cfc2c147993078dd9a33565c17879a2b0c5d152c
Enzo Godoy <enzo.godoy@go-it.work>
2023-02-06 10:41:45 -0300

    LicenseeMap finished

---
+++ src/EasyDocs.Infra.Data/Mappings/LicenseeMap.cs
@@ -0,0 +1,20 @@
+﻿using EasyDocs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyDocs.Infra.Data.Mappings;
+
+public sealed class LicenseeMap : IEntityTypeConfiguration<Licensee>
+{
+    public void Configure(EntityTypeBuilder<Licensee> builder)
+    {
+        builder.HasKey(l => l.Id);
+
+        builder.OwnsOne(l => l.Description, description =>
+        {
+            description.Property(d => d.Text)
+            .HasColumnType("varchar(150)")
+            .HasColumnName("Description");
+        });
+    }
+}
