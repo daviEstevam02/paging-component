abe32ec561233acccd25071e0a06734b4704c5c2
EnzoFNGodoy <enzofngodoy@hotmail.com>
2023-02-05 23:55:12 -0300

    Source value object finished

---
+++ src/EasyDocs.Domain/Entities/Document.cs
@@ -0,0 +1,23 @@
+﻿using EasyDocs.Domain.Core.Entities;
+using EasyDocs.Domain.Enums;
+using EasyDocs.Domain.ValueObjects;
+
+namespace EasyDocs.Domain.Entities;
+
+public sealed class Document : Entity
+{
+    private Document()
+    { }
+
+    public Guid LicenseeId { get; private set; }
+    public Guid CompanyId { get; private set; }
+    public Guid DocumentTypeId { get; private set; }
+    public Description Description { get; private set; } = null!;
+    public Source Source { get; private set; } = null!;
+    public string File { get; private set; } = string.Empty;
+    public bool SpecificAccess { get; set; }
+
+    public DocumentType DocumentType { get; private set; } = null!;
+    public Company Company { get; private set; } = null!;
+    public Licensee Licensee { get; private set; } = null!;
+}
---
+++ src/EasyDocs.Domain/ValueObjects/Source.cs
@@ -0,0 +1,25 @@
+﻿using EasyDocs.Domain.Core.ValueObjects;
+using Flunt.Validations;
+
+namespace EasyDocs.Domain.ValueObjects;
+
+public sealed class Source : ValueObject
+{
+    private Source()
+    { }
+
+    public Source(string text)
+    {
+        Text = text;
+
+        AddNotifications(new Contract<Source>()
+            .Requires()
+             .IsNotNullOrEmpty(Text, "Source.Text", "A origem não pode ser vazia.")
+            .IsNotNullOrWhiteSpace(Text, "Source.Text", "A origem não pode ser vazia.")
+            .IsLowerOrEqualsThan(5, Text.Length, "Source.Text", "A origem não deve conter menos de 5 caracteres.")
+            .IsGreaterOrEqualsThan(250, Text.Length, "Source.Text", "A origem não deve conter mais de 250 caracteres.")
+            );
+    }
+
+    public string Text { get; private set; } = string.Empty;
+}
