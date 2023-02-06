c597f883a2b7497a003f4c532ff8562a00896426
Enzo Godoy <enzo.godoy@go-it.work>
2023-02-06 11:20:02 -0300

    DocumentTypeMap finished

---
+++ src/EasyDocs.Infra.Data/Mappings/DocumentTypeMap.cs
@@ -0,0 +1,28 @@
+﻿using EasyDocs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyDocs.Infra.Data.Mappings;
+
+public sealed class DocumentTypeMap : IEntityTypeConfiguration<DocumentType>
+{
+    public void Configure(EntityTypeBuilder<DocumentType> builder)
+    {
+        builder.HasKey(dt => dt.Id);
+
+        builder.HasOne(dt => dt.Licensee)
+            .WithMany(l => l.DocumentTypes)
+            .HasForeignKey(dt => dt.LicenseeId);
+
+        builder.HasOne(dt => dt.Company)
+            .WithMany(l => l.DocumentTypes)
+            .HasForeignKey(dt => dt.CompanyId);
+
+        builder.OwnsOne(dt => dt.Description, description =>
+        {
+            description.Property(d => d.Text)
+            .HasColumnType("varchar(150)")
+            .HasColumnName("Description");
+        });
+    }
+}
