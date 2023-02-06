3d80505ab32f0cfa59e53396a63dd7ef2d6e0253
Enzo Godoy <enzo.godoy@go-it.work>
2023-02-06 11:27:05 -0300

    UserDocumentMap finished

---
+++ src/EasyDocs.Infra.Data/Mappings/UserDocumentMap.cs
@@ -0,0 +1,29 @@
+﻿using EasyDocs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyDocs.Infra.Data.Mappings;
+
+public sealed class UserDocumentMap : IEntityTypeConfiguration<UserDocument>
+{
+    public void Configure(EntityTypeBuilder<UserDocument> builder)
+    {
+        builder.HasKey(ud => new { ud.UserId, ud.DocumentId });
+
+        builder.HasOne(ud => ud.Licensee)
+            .WithMany(l => l.UserDocuments)
+            .HasForeignKey(ud => ud.LicenseeId);
+
+        builder.HasOne(ud => ud.Company)
+            .WithMany(c => c.UserDocuments)
+            .HasForeignKey(ud => ud.CompanyId);
+
+        builder.HasOne(ud => ud.User)
+            .WithMany(u => u.UserDocuments)
+            .HasForeignKey(ud => ud.UserId);
+
+        builder.HasOne(ud => ud.Document)
+            .WithMany(d => d.UserDocuments)
+            .HasForeignKey(ud => ud.DocumentId);
+    }
+}
