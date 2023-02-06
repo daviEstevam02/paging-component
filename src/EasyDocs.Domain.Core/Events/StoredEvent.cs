209184c913a2a7f5b374d04caaabf3dea626cfcc
EnzoFNGodoy <enzofngodoy@hotmail.com>
2023-02-05 21:48:10 -0300

    Starting to build the EFCore structure

---
+++ src/EasyDocs.Domain.Core/Events/StoredEvent.cs
@@ -0,0 +1,23 @@
+﻿using NetDevPack.Messaging;
+
+namespace Equinox.Domain.Core.Events;
+
+public class StoredEvent : Event
+{
+    protected StoredEvent() { }
+
+    public StoredEvent(Event theEvent, string data, string user)
+    {
+        Id = Guid.NewGuid();
+        AggregateId = theEvent.AggregateId;
+        MessageType = theEvent.MessageType;
+        Data = data;
+        User = user;
+    }
+
+    public Guid Id { get; private set; }
+
+    public string Data { get; private set; } = string.Empty;
+
+    public string User { get; private set; } = string.Empty;
+}
