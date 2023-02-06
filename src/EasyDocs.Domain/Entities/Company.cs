ac6cee62eb49c62b35011d9852cc3eef507245f5
EnzoFNGodoy <enzofngodoy@hotmail.com>
2023-02-05 23:24:58 -0300

    CNPJ value object finished

--- src/EasyDocs.Domain/Entities/Company.cs
+++ src/EasyDocs.Domain/Entities/Company.cs
@@ -3,18 +3,18 @@ using EasyDocs.Domain.ValueObjects;
 
 namespace EasyDocs.Domain.Entities;
 
-public sealed class Company : BaseEntity
+public sealed class Company : Entity
 {
     private Company()
     { }
 
     public Guid LicenseeId { get; private set; }
-    public FantasyName FantasyName { get; private set; }
-    public LegalName LegalName { get; private set; }
-    public Address Address { get; private set; }
-    public Phone Contact { get; private set; }
-    public CNPJ Cnpj { get; private set; }
+    public FantasyName FantasyName { get; private set; } = null!;
+    public LegalName LegalName { get; private set; } = null!;
+    public Address Address { get; private set; } = null!;
+    public Phone Contact { get; private set; } = null!;
+    public CNPJ Cnpj { get; private set; } = null!;
     public bool IsHeadquarter { get; private set; }
 
-    public Licensee Licensee { get; private set; }
+    public Licensee Licensee { get; private set; } = null!;
 }
---
+++ src/EasyDocs.Domain/ValueObjects/CNPJ.cs
@@ -0,0 +1,24 @@
+﻿using EasyDocs.Domain.Core.ValueObjects;
+using Flunt.Extensions.Br.Validations;
+using Flunt.Validations;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+
+namespace EasyDocs.Domain.ValueObjects;
+
+public sealed class CNPJ : ValueObject
+{
+    private CNPJ()
+    { }
+
+    public CNPJ(string number)
+    {
+        Number = number;
+
+        AddNotifications(new Contract<CNPJ>()
+            .Requires()
+            .IsCnpj(Number, "CNPJ.Number", "CNPJ inválido.")
+            );
+    }
+
+    public string Number { get; private set; } = string.Empty;  
+}
