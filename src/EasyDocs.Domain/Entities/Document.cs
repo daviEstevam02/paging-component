5c9e26de5e264fba90bf38f15e5bf0eaeab6ccf5
EnzoFNGodoy <enzofngodoy@hotmail.com>
2023-02-05 23:58:09 -0300

    Document entity finished

--- src/EasyDocs.Domain/Entities/Document.cs
+++ src/EasyDocs.Domain/Entities/Document.cs
@@ -9,12 +9,34 @@ public sealed class Document : Entity
     private Document()
     { }
 
+    public Document(
+        Guid id, 
+        Guid licenseeId, 
+        Guid companyId, 
+        Guid documentTypeId, 
+        Description description, 
+        Source source, 
+        byte[]? file, 
+        bool specificAccess
+        ) : base(id)
+    {
+        LicenseeId = licenseeId;
+        CompanyId = companyId;
+        DocumentTypeId = documentTypeId;
+        Description = description;
+        Source = source;
+        File = file;
+        SpecificAccess = specificAccess;
+
+        AddNotifications(Description, Source);
+    }
+
     public Guid LicenseeId { get; private set; }
     public Guid CompanyId { get; private set; }
     public Guid DocumentTypeId { get; private set; }
     public Description Description { get; private set; } = null!;
     public Source Source { get; private set; } = null!;
-    public string File { get; private set; } = string.Empty;
+    public byte[]? File { get; private set; }
     public bool SpecificAccess { get; set; }
 
     public DocumentType DocumentType { get; private set; } = null!;
