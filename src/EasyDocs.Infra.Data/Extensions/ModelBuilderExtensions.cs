a57e8d7465d22a9f949dbbcb1987dec286aa3447
EnzoFNGodoy <enzofngodoy@hotmail.com>
2023-02-06 00:18:26 -0300

    Starting to mapping the entities. todo: Mappings, GenericRepositories and Migrations/UpdateDatabase

---
+++ src/EasyDocs.Infra.Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EasyDocs.Infra.Data.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void ConfigureMappings(this ModelBuilder modelBuilder)
+    {
+    }
+}
