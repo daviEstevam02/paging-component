a95b24f10b292c3195fc134b0ecc9e800d640799
FelipePox <felipe.cardoso@go-it.work>
2023-02-13 16:41:49 -0300

    UserDocument Handler update

--- src/EasyDocs.Domain/Handlers/UserDocuments/UserDocumentCommandHandler.cs
+++ src/EasyDocs.Domain/Handlers/UserDocuments/UserDocumentCommandHandler.cs
@@ -5,6 +5,7 @@ using EasyDocs.Domain.Entities;
 using EasyDocs.Domain.Events.UserDocuments;
 using EasyDocs.Domain.Interfaces;
 using MediatR;
+using System.Xml.Linq;
 
 namespace EasyDocs.Domain.Handlers.UserDocuments;
 
@@ -46,8 +47,8 @@ public sealed class UserDocumentCommandHandler : CommandHandler<UserDocument>,
             AddNotification("Empresa", "Uma empresa com esse Id não existe.");
             return new CommandResult(false, Notifications.ToList());
         }
-        var user = await _userRepository.GetOneWhere(u => u.Id == command.UserId);
 
+        var user = await _userRepository.GetOneWhere(u => u.Id == command.UserId);
         if (user is null)
         {
             AddNotification("User", "Um usuário com esse Id não existe.");
@@ -77,13 +78,63 @@ public sealed class UserDocumentCommandHandler : CommandHandler<UserDocument>,
 
         _userDocumentRepository.Add(userDocument);
 
-        return await Commit(_userDocumentRepository.UnitOfWork, "Tipo de usuario documento criado com sucesso!");
+        return await Commit(_userDocumentRepository.UnitOfWork, "Tipo de documento usuário criado com sucesso!");
 
     }
 
-    public Task<CommandResult> Handle(UpdateUserDocumentCommand request, CancellationToken cancellationToken)
+    public async Task<CommandResult> Handle(UpdateUserDocumentCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var existentUserDocument = await _userDocumentRepository.GetOneWhere(dt => dt.Id == command.Id);
+        if (existentUserDocument is null)
+        {
+            AddNotification("UserDocument", "Um tipo de documento usuário com esse Id não existe.");
+            return new CommandResult(false, Notifications.ToList());
+        }
+
+        if (!await _licenseeRepository.LicenseeExists(command.LicenseeId))
+        {
+            AddNotification("Licensee", "Um licenciado com esse Id não existe.");
+            return new CommandResult(false, Notifications.ToList());
+        }
+
+        if (!await _companyRepository.CompanyExists(command.CompanyId))
+        {
+            AddNotification("Empresa", "Uma empresa com esse Id não existe.");
+            return new CommandResult(false, Notifications.ToList());
+        }
+
+        var user = await _userRepository.GetOneWhere(u => u.Id == command.UserId);
+        if (user is null)
+        {
+            AddNotification("User", "Um usuário com esse Id não existe.");
+            return new CommandResult(false, Notifications.ToList());
+        }
+
+        command.Validate();
+        if (!command.IsValid) return new CommandResult(false, command.Notifications.ToList());
+
+        var userDocument = new UserDocument(
+            Guid.NewGuid(),
+            user.Id,
+            command.DocumentId,
+            command.CompanyId,
+            command.LicenseeId
+        );
+
+        userDocument.Update(existentUserDocument.CreatedAt);
+        if (!userDocument.IsValid) return new CommandResult(false, userDocument.Notifications.ToList());
+
+        userDocument.AddDomainEvent(new UserDocumentCreatedEvent(
+           userDocument.Id,
+           user.Id,
+           userDocument.DocumentId,
+           userDocument.CompanyId,
+           userDocument.LicenseeId,
+           user.Username.ToString()));
+
+        _userDocumentRepository.Update(userDocument.Id, userDocument);
+        return await Commit(_userDocumentRepository.UnitOfWork, "Tipo de documento usuário atualizado com sucesso!");
+
     }
 
     public Task<CommandResult> Handle(DeleteUserDocumentCommand request, CancellationToken cancellationToken)
