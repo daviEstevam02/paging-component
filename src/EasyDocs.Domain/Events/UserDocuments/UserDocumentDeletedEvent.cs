0740ad8b6d607fd336ac0aa213140b9278412ba6
FelipePox <felipe.cardoso@go-it.work>
2023-02-13 15:20:56 -0300

    UserDocumment Event

---
+++ src/EasyDocs.Domain/Events/UserDocuments/UserDocumentDeletedEvent.cs
@@ -0,0 +1,20 @@
+﻿using EasyDocs.Domain.Core.Events;
+using EasyDocs.Domain.Core.Messaging;
+using EasyDocs.Domain.Helpers;
+
+namespace EasyDocs.Domain.Events.UserDocuments;
+
+public sealed class UserDocumentDeletedEvent : Event
+{
+    public UserDocumentDeletedEvent(
+       Guid id,
+       Guid userId,
+       string username
+    ) : base(EAction.Deleted, userId, username, EntitiesContexts.DOCUMENT_TYPES)
+    {
+        Id = id;
+        AggregateId = id;
+    }
+
+    public Guid Id { get; private set; }
+}
