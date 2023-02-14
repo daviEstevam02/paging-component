bfc7155038abd67a2b468981eeef38889c956ada
FelipePox <felipe.cardoso@go-it.work>
2023-02-14 10:32:14 -0300

    Controllers de company e UserDocuments finalizados

--- src/EasyDocs.WebApi/Controllers/CompanyController.cs
+++ src/EasyDocs.WebApi/Controllers/CompanyController.cs
@@ -25,6 +25,16 @@ public sealed class CompanyController : ApiController
     public async Task<IEnumerable<ResponseCompanyViewModel>> GetAll(Guid licenciadoId)
        => await _companyServices.GetAll(licenciadoId);
 
+
+    /// <summary>
+    /// Retorna a empresa com o código especificado dela
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    [HttpGet("{companyId}")]
+    public async Task<ResponseCompanyViewModel> GetById(Guid companyId)
+       => await _companyServices.GetById(companyId);
+
     /// <summary>
     /// Realiza a criação de uma Empresa, retornando uma mensagem. Caso haja falha, o retorno será uma lista de erros.
     /// </summary>
@@ -33,4 +43,22 @@ public sealed class CompanyController : ApiController
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] PostCompanyViewModel viewModel)
        => CustomResponse(await _companyServices.Create(viewModel));
+
+    /// <summary>
+    /// Realiza a edição de uma Empresa através de um corpo JSON, retornando uma mensagem. Caso haja falha, o retorno será uma lista de erros.
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    [HttpPut]
+    public async Task<IActionResult> Put([FromForm] PutCompanyViewModel viewModel)
+        => CustomResponse(await _companyServices.Update(viewModel));
+
+    /// <summary>
+    /// Realiza a deleção (desativamento) de uma Empresa através de um corpo JSON, retornando uma mensagem. Caso haja falha, o retorno será uma lista de erros.
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteCompanyViewModel viewModel)
+        => CustomResponse(await _companyServices.Delete(viewModel));
 }
---
+++ src/EasyDocs.WebApi/Controllers/UserDocumentController.cs
@@ -0,0 +1,65 @@
+﻿using EasyDocs.Application.Interfaces;
+using EasyDocs.Application.Services;
+using EasyDocs.Application.ViewModels.Documents;
+using EasyDocs.Application.ViewModels.UserDocuments;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyDocs.WebApi.Controllers;
+[Authorize]
+[Route("userDocuments")]
+public sealed class UserDocumentController : ApiController
+{
+    private readonly IUserDocumentServices _userDocumentServices;
+
+	public UserDocumentController(IUserDocumentServices userDocumentServices)
+	{
+		_userDocumentServices = userDocumentServices;
+	}
+
+
+    /// <summary>
+    /// Retorna todos os Usuarios por documentos cadastrados interligados com a Empresa especificada.
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    [HttpGet("all/{companyId}")]
+    public  async Task<IEnumerable<ResponseUserDocumentViewModel>> GetAll(Guid companyId)
+       => await _userDocumentServices.GetAll(companyId);
+
+    /// <summary>
+    /// Retorna o  Usuário por documento com o código especificado na URL.
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    [HttpGet("{userDocumentId}")]
+    public async Task<ResponseUserDocumentViewModel> GetById(Guid userDocumentId)
+       => await _userDocumentServices.GetById(userDocumentId);
+
+    /// <summary>
+    /// Realiza a criação de um Usuário por document através de um Form, retornando uma mensagem. Caso haja falha, o retorno será uma lista de erros.
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    [HttpPost]
+    public async Task<IActionResult> Post([FromForm] PostUserDocumentViewModel viewModel)
+        => CustomResponse(await _userDocumentServices.Create(viewModel));
+
+    /// <summary>
+    /// Realiza a edição de um Uusário por documento através de um corpo JSON, retornando uma mensagem. Caso haja falha, o retorno será uma lista de erros.
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    [HttpPut]
+    public async Task<IActionResult> Put([FromForm] PutUserDocumentViewModel viewModel)
+        => CustomResponse(await _userDocumentServices.Update(viewModel));
+
+    /// <summary>
+    /// Realiza a deleção (desativamento) de um Usuário por documento através de um corpo JSON, retornando uma mensagem. Caso haja falha, o retorno será uma lista de erros.
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteUserDocumentViewModel viewModel)
+        => CustomResponse(await _userDocumentServices.Delete(viewModel));
+}
